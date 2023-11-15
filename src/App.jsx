d482c6b83fe30c9f4e7673d790ed40f7bcf2394e
Natalia Macias <natajenkins@hotmail.com>
2023-11-14 23:52:05 -0600

    mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
 import "./App.scss";
+import { useState } from "react";
 import Logo from "./assets/logo.svg";
 import Arrow from "./assets/icon-arrow.svg";
-import Error from "./assets/icon-error.svg";
+import ErrorIcon from "./assets/icon-error.svg";
 import ImgDesktop from "./assets/hero-desktop.jpg";
 import ImgMobile from "./assets/hero-mobile.jpg";
 
 function App() {
-    const handleSubmit = () => {};
+    const [formData, setFormData] = useState({
+        // name: "",
+        email: "",
+    });
+
+    const [formErrors, setFormErrors] = useState({
+        // name: "",
+        email: "",
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const errors = {};
+
+        // if (!formData.name.trim()) {
+        //     errors.name = "Name is required";
+        // }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!formData.email.trim() || !emailRegex.test(formData.email)) {
+            errors.email = "Please provide a valid email";
+        }
+
+        setFormErrors(errors);
+
+        if (Object.keys(errors).length === 0) {
+            console.log("Form submitted:", formData);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+
+        setFormErrors({
+            ...formErrors,
+            [name]: "",
+        });
+    };
+
     return (
         <main>
             <div className="container">
+                <img src={Logo} alt="" className="logo-mobile" />
                 <div className="left">
                     <div className="left-content">
                         <img src={Logo} alt="" className="logo" />
@@ -23,11 +69,32 @@ function App() {
                             up-to-date with announcements and our launch deals.`}
                             </p>
                             <div className="input">
-                                <input
-                                    type="email"
-                                    placeholder="Email Address"
-                                />
-                                <form action="">
+                                <form onSubmit={handleSubmit}>
+                                    <input
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        type="text"
+                                        id="email"
+                                        name="email"
+                                        placeholder="Email Address"
+                                        className={
+                                            formErrors.email
+                                                ? "error"
+                                                : undefined
+                                        }
+                                    />
+                                    {formErrors.email && (
+                                        <>
+                                            <span className="error-span">
+                                                {formErrors.email}
+                                            </span>
+                                            <img
+                                                src={ErrorIcon}
+                                                alt=""
+                                                className="error-icon"
+                                            />
+                                        </>
+                                    )}
                                     <button type="submit">
                                         <img src={Arrow} alt="" />
                                     </button>
